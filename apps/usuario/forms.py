5b9250fbca00193550ef948166ba7a531ae381a1
sylar <seba.sylar@gmail.com>
2018-10-31 11:27:51 -0300

    Nuevo Mis perris

---
+++ apps/usuario/forms.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegistroForm(UserCreationForm):
+
+	class Meta:
+		model = User
+		fields = [
+				'username',
+				'first_name',
+				'last_name',
+				'email',
+             ###'run',
+             ###'fNaci',
+             ###'telefono',
+             ###'region',
+             ###'ciudad',
+             ###'tipoCasa'
+			]
+		labels = {
+				'username': 'Nombre de usuario',
+				'first_name': 'Nombre',
+				'last_name': 'Apellidos',
+				'email': 'Correo',
+                #'run' : 'Run',
+                #'fNaci' : 'Fecha nacimiento',
+                #'telefono' : 'Telefono',
+                #'region' : 'Region',
+                #'ciudad' : 'Ciudad',
+                #'tipoCasa' : 'Tipo de vivienda',
+
+		}
